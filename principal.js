efb34747b3a92338c4fb79983dfd54791a60bdd9
Suelle Silva <suellesilva91@gmail.com>
2025-01-29 23:31:10 -0300

    arquivo  em javascript

--- principal.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $('#header').load('../src/componentes/header.html');
-    $('#footer').load('../src/componentes/footer.html');
-});
-
-$(document).ready(function() {
-    // Carregar o conteúdo do arquivo loja.html dentro da div #loja
-    $("#loja").load("../src/paginas/loja.html");
-});
